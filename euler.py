1028ef1f122fd0e1e7905f683016d5183de91a4d
evan-i-cohen <evan-i-cohen@berkeley.edus>
2015-09-24 10:00:12 -0700

    started implementation of heuletzer alg

--- euler.py
+++ euler.py
@@ -1,4 +1,5 @@
 import openbabel as ob
+import pybel as pb
 
 def exists_eulerian (molecule):
     for atom in molecule.atoms:
@@ -8,9 +9,24 @@ def exists_eulerian (molecule):
         if heavyvalence % 2 != True:
             return False;  # Eulerian path doesn't exist
     return True; 
-def find_eulerian(molecule):
-    if not exists_eulerian(molecule):
+def find_eulerian(mol):
+	path = []
+    if not exists_eulerian(mol):
     	print "No eulerian path exists for that"
+   	else:
+   		#Hierholtzer's ALGORITHM  for finding Eulerian path
+   		mol_no_H = mol.removeh()
+   		obmol = mol_no_H.OBMol
+   		used_edges = set()
+   		obAtom start_atom_ptr = obmol.index(1)
+   		Bonds = start_atom_ptr.Bonds()
+   		temp_path = []
+   		final_path = []
+   		visited_edges =set()
+   		#while next_atom_ptr.idx != start_atom_ptr.idx:
+   		for  obatom in openbabel.OBMolAtomIter(obmol):
+   			if obatom in  visited_edges
+   				continue
      
 
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,2 +1,7 @@
-#Test 1
-main("benzene.xyz")
+
+#test 1 
+import main
+import openbabel as ob
+import pybel as pb
+mol = pb.readfile("xyz", "benzene.xyz").next()
+main(mol) 
