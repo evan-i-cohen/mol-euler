6167ce1217dd6dcbe99e44e8108c39e2ab7e51c6
evan-i-cohen <evan-i-cohen@berkeley.edus>
2015-12-16 10:33:02 -0800

    changed details of animation... now highlights bonds in yellow and rotates molecule while animating eulerian tour for better visibility of tour

--- graphics.py
+++ graphics.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import openbabel as ob
 from visual import *
+mol_frame= frame()
 def set_background_color(r, g, b):
 	display.get_selected().background =(r,g,b)
 def get_color(atom):
@@ -22,8 +23,7 @@ def display_atoms(mol):
 		atom_pos = get_coords(atom)
 		atom_color = get_atom_color(atom)
 		atom_radius = 0.5*ob.OBElementTable().GetCovalentRad(atom.GetAtomicNum())
-		#print(atom_radius)
-		sphere(pos = atom_pos, radius =atom_radius, color = atom_color);
+		sphere(frame = mol_frame, pos = atom_pos, radius =atom_radius, color = atom_color);
 def get_atom_color(atom):
 	return ob.OBElementTable().GetRGB(atom.GetAtomicNum())
 
@@ -41,22 +41,28 @@ def display_bond (atom1, atom2, bond_color = None):
         bondVector = atom2Coords - atom1Coords
         if bond_color == None:
         	bond_color = get_bond_color(atom1, atom2)
-    	cylinder(pos=atom1Coords, axis=bondVector, radius=0.05, color = bond_color)
+    	cylinder(frame = mol_frame, pos=atom1Coords, axis=bondVector, radius=0.05, color = bond_color)
 def display_bonds(mol):
 		 ob_mol = mol.OBMol
 		 for bond in ob.OBMolBondIter(ob_mol):
 		     print(bond.GetId())
 		     atom1 = bond.GetBeginAtom()
 		     atom2 = bond.GetEndAtom()
-		     display_bond(atom1, atom2)
+		     display_bond(atom1, atom2, (1,1,1))
 def animate(cycle):
-	for atom1, atom2 in zip( cycle[:], cycle[1:] + [cycle[0]] ):
-		rate(5)
+	sleep(20)
+	for atom1, atom2 in zip( cycle[:len(cycle)], cycle[1:]):
+		sleep(1)
 		bond_color = get_bond_color(atom1, atom2)
-		inverse_color = tuple( map(lambda s: 255-s, bond_color))
-		display_bond(atom1, atom2, inverse_color)
+		inverse_color = (1,1,0)
+		display_bond(atom1, atom2, (inverse_color))
+		rotate_mol_animate(pi, 20, 100)
 
-
+def rotate_mol_animate(tot_rotation, time_per_step, num_steps):
+	for a in range(num_steps):
+		rate(time_per_step)
+		angle_per_step = tot_rotation/num_steps
+		mol_frame.rotate(angle=angle_per_step, axis=mol_frame.axis, pos = mol_frame.pos)
 
 
 
